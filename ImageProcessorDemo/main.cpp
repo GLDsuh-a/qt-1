0a642fa56f71d94e7f815305a806173c8e586ddb
yiminyangguang520 <ltz121320@163.com>
2015-08-28 11:08:13 +0800

    add qt demo

---
+++ ImageProcessorDemo/main.cpp
@@ -0,0 +1,11 @@
+#include "imgprocessor.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    ImgProcessor w;
+    w.show();
+
+    return a.exec();
+}
